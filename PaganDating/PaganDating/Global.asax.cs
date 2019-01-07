3f884db5dc5e01c8b1434f8b8d1dbc5e0a554d58
Anders Husmo <anders.k.husmo@gmail.com>
2019-01-07 12:19:59 +0100

    Installerat nuget WebApi

--- PaganDating/PaganDating/Global.asax.cs
+++ PaganDating/PaganDating/Global.asax.cs
@@ -16,6 +16,7 @@ namespace PaganDating
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register); 
         }
     }
 }
