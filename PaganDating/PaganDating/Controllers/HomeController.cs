fe60d9cf025ad3623d702bc4e823f0e48112fa2d
Hampus <lampedampe@gmail.com>
2019-01-11 16:52:13 +0100

    Listar användare vid start

--- PaganDating/PaganDating/Controllers/HomeController.cs
+++ PaganDating/PaganDating/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DataLayer;
 using Microsoft.AspNet.Identity;
+using PaganDating.Models;
 
 namespace PaganDating.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        private PaganDatingModelContainer db = new PaganDatingModelContainer();
+
+        public ActionResult UserIndex()
         {
             var db = new PaganDatingModelContainer();
             var accountId = User.Identity.GetUserId();
@@ -34,6 +39,42 @@ namespace PaganDating.Controllers
             return View();
         }
 
+        public ActionResult Details(int? id)
+        {
+            var viewModel = new UserDetailsViewModel(db.UserSet.Find(id));
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (viewModel.User == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(viewModel);
+        }
+
+        public ActionResult Index(string searchString)
+        {
+            var users = db.UserSet.ToList();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                users = db.UserSet.Where(u => u.Name.Contains(searchString)).ToList();
+            }
+
+            return View(users);
+        }
+
+        [HttpGet]
+        public ActionResult Search(string searchString)
+        {
+            var users = db.UserSet.Where(u => u.Name.Contains(searchString));
+            return View(users.ToList());
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -41,12 +82,7 @@ namespace PaganDating.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
 
         public ActionResult ProfileRedirect()
         {
